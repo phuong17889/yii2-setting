0d9cc0563429788bd2060d4d4f22f440b529074a
Lê Phương <notteen@gmail.com>
2016-05-02 18:16:38 +0700

    fix view

--- src/Setting.php
+++ src/Setting.php
@@ -1,25 +1,21 @@
 <?php
-
 namespace navatech\setting;
 
-use Yii;
 use navatech\setting\models\Setting as SettingModel;
+use Yii;
+use yii\base\Component;
 
-class Setting extends \yii\base\Component
-{
-    public function get($code, $default = null) {
+class Setting extends Component {
 
-        if (!$code) {
-            return $default;
-        }
-
-        $setting = SettingModel::find()->where(['code' => $code])->one();
-
-        if ($setting) {
-            return $setting->value;
-        } else {
-            return $default;
-        }
-    }
-
+	public function get($code, $default = null) {
+		if (!$code) {
+			return $default;
+		}
+		$setting = SettingModel::findOne(['code' => $code]);
+		if ($setting) {
+			return $setting->value;
+		} else {
+			return $default;
+		}
+	}
 }
