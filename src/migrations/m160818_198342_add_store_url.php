489476d36658b2e2a0e6a3c349d1df265bb431cc
Le Phuong <phuong17889@gmail.com>
2016-08-18 17:24:54 +0700

    add migrate

---
+++ src/migrations/m160818_198342_add_store_url.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: phuon
+ * Date: 8/18/2016
+ * Time: 5:14 PM
+ */
+class m160818_198342_add_store_url extends \yii\db\Migration {
+
+	public function safeUp() {
+		$this->addColumn('{{%setting}}', 'store_url', \yii\db\mysql\Schema::TYPE_STRING . '(255) NULL');
+	}
+}
